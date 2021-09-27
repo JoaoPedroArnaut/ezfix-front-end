6fc1aa29bb3d6a07c6832ce1a18345cb21169e80
João Pedro Arnaut <joao.silva@bandtec.com.br>
2021-09-27 14:58:37 -0300

    bug (#2)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 function Home(){
     return (
-    <div className="container flex content-around justify-center max-w-full text-indigo">hello world</div>
+    <div className="container flex content-around justify-center max-w-full text-indigo-dark">hello world</div>
     )
 }
 
