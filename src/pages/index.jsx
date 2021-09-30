e4b687d243ba0f325f6d348ba55761412113069f
JoaoPedroArnaut <joao.silva@bandtec.com.br>
2021-09-30 18:09:15 -0300

    nsei

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,10 +3,9 @@ import Navbar from "../components/Navbar"
 function Home() {
     return (
         <>
-    <Navbar botoes={["Solução","Diferenciais","Sobre","Contato"]} login="login/cadastro" />
-        <div className = "container flex content-around justify-center max-w-full text-blue-dark" > 
-         hello world 
-        </div>
+            <Navbar botoes={["Solução","Diferenciais","Sobre","Contato"]} login="login/cadastro" />
+            <div className = "container flex content-around justify-center max-w-full text-blue-dark" > 
+            </div>
         </>
     )
 }
