8c0fa80e090f51a64da8682c9cb9d61a4131a077
JoaoPedroArnaut <joao.silva@bandtec.com.br>
2021-10-06 04:17:07 -0300

    footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = (props) => {
     
     return (
         <div className="bg-blue h-24 flex justify-around border-black border-solid border-b-2 w-full fixed">
-            <div className="flex justify-between items-center container w-4/5">
+            <div className="flex justify-between items-center container w-11/12 sm:w-4/5">
             <Image src="/ezfix_logo.png" width="150px" height="150px" alt="logo ezfix" />
             <div>
                 <span className="text-white mr-4">{props.login[1]}</span>
--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -4,7 +4,7 @@ const Section3 = () => {
     return (
         <div className="alturaSemNavbar grid lg:grid-cols-3 lg:grid-rows-3 grid-cols-3 grid-rows-5">
             <div className="w-full h-15 flex justify-around sm:row-start-1 sm:row-span-1 sm:col-start-1 sm:col-span-2 row-start-4 row-span-1 col-start-1 col-span-3">
-                <div className="w-4/5 lg:pt-8 sm:pt-4 xl:pt-12">
+                <div className="w-4/6 lg:pt-8 sm:pt-4 xl:pt-12">
                     <h1 className="lg:text-3xl xl:text-4xl sm:text-2xl  font-bold">Tenha seu equipamento reparado sem sair de casa!</h1>
                     <p className="lg:text-xl xl:text-3xl sm:text-xl lg:w-1/2 mt-4">A Lalamove é nossa parceira oficial</p>
                 </div>
