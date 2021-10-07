47c426ce6c8ac9625b7ae4900fb17d79a78586b0
JoaoPedroArnaut <joao.silva@bandtec.com.br>
2021-10-07 17:44:53 -0300

    meio maracana

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -3,8 +3,8 @@ import FormProduto from './FormProduto'
 
 const Section2 = () => {
     return (
-        <div className="w-full tamanhoMinimo flex justify-around">
-            <div className="container h-full h-fit pt-8 sm:py-8  flex items-center lg:justify-between justify-around w-full sm:w-4/5">
+        <div className="w-full sm:my-24 tamanhoMinimo flex justify-around">
+            <div className="container h-full h-fit pt-8 sm:py-8  flex items-center lg:justify-around justify-around w-full sm:w-4/5">
                 <img src="/undraw_Questions_re_1fy7.svg" alt="" className="xl:w-2/4 lg:w-2/5 hidden lg:block" />
                 <FormProduto />
             </div>
--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Section3 = () => {
     return (
-        <div className="alturaSemNavbar sm:mt-44 grid lg:grid-cols-3 lg:grid-rows-3 grid-cols-3 grid-rows-5">
+        <div className="alturaSemNavbar grid lg:grid-cols-3 lg:grid-rows-3 grid-cols-3 grid-rows-5">
             <div className="w-full h-15 flex justify-around sm:row-start-1 sm:row-span-1 sm:col-start-1 sm:col-span-2 row-start-4 row-span-1 col-start-1 col-span-3">
                 <div className="w-4/6 lg:pt-8 sm:pt-4 xl:pt-12">
                     <h1 className="lg:text-3xl xl:text-4xl sm:text-2xl  font-bold">Tenha seu equipamento reparado sem sair de casa!</h1>
