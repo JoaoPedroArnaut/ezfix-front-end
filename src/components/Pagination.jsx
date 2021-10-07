cf38bca5dc50dd191681744842ba9f518c7f3830
JoaoPedroArnaut <joao.silva@bandtec.com.br>
2021-10-07 20:09:34 -0300

    tela assistencia semi pronta

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,7 +4,7 @@ const Pagination = () => {
     return (
         <div class="flex justify-around my-12 w-full">
             <div class="flex text-black w-4/12">
-                <div class="w-14 h-12 mr-1 flex justify-center items-center rounded-full bg-blue-dark text-white cursor-pointer">
+                <div class="w-14 h-12 mr-1 flex justify-center items-center rounded-full bg-blue-light text-black cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left w-6 h-6">
                         <polyline points="15 18 9 12 15 6"></polyline>
                     </svg>
--- src/pages/assistencias.jsx
+++ src/pages/assistencias.jsx
@@ -13,14 +13,32 @@ const assistencias = () => {
         <>
             <Navbar login={["Login", "Criar conta"]} />
             <Recomendadas />
-            <div className="flex justify-around w-full mt-8">
-                <div className="">
-                    <input type="text" className="bg-blue-dark h-8 text-white rounded-l-md p-2" placeholder="pesquisar" />
-                    <button className="text-white bg-blue-dark p-1 rounded-r-md h-8"><FontAwesomeIcon icon={faSearch}/></button>
+            <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center sm:flex-row sm:justify-around w-full my-8">
+                    <div className="">
+                        <input type="text" className="bg-blue-dark h-8 text-white rounded-l-md p-2" placeholder="pesquisar" />
+                        <button className="text-white bg-blue-dark p-1 rounded-r-md h-8"><FontAwesomeIcon icon={faSearch} /></button>
+                    </div>
+                    <ComboBox escuro='true' label="Filtro" opicao={["filtro"]} />
+
                 </div>
-                <ComboBox escuro='true' label="Filtro" opicao={["filtro"]}/>
+                <hr className="text-black w-full lg:w-4/5 text-opacity-25" />
+                <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 grid-rows-4 gap-8 w-4/5 mt-8">
+                    <CardAssistencia nome="wcl phones" avaliacao="4,5" endereco="Grajau - são paulo" categorias="Celular" />
+                    <CardAssistencia nome="wcl phones" avaliacao="4,5" endereco="Grajau - são paulo" categorias="Celular" />
+                    <CardAssistencia nome="wcl phones" avaliacao="4,5" endereco="Grajau - são paulo" categorias="Celular" />
+                    <CardAssistencia nome="wcl phones" avaliacao="4,5" endereco="Grajau - são paulo" categorias="Celular" />
+                    <CardAssistencia nome="wcl phones" avaliacao="4,5" endereco="Grajau - são paulo" categorias="Celular" />
+                    <CardAssistencia nome="wcl phones" avaliacao="4,5" endereco="Grajau - são paulo" categorias="Celular" />
+                    <CardAssistencia nome="wcl phones" avaliacao="4,5" endereco="Grajau - são paulo" categorias="Celular" />
+                    <CardAssistencia nome="wcl phones" avaliacao="4,5" endereco="Grajau - são paulo" categorias="Celular" />
+                    <CardAssistencia nome="wcl phones" avaliacao="4,5" endereco="Grajau - são paulo" categorias="Celular" />
+                    <CardAssistencia nome="wcl phones" avaliacao="4,5" endereco="Grajau - são paulo" categorias="Celular" />
+                    <CardAssistencia nome="wcl phones" avaliacao="4,5" endereco="Grajau - são paulo" categorias="Celular" />
+                    <CardAssistencia nome="wcl phones" avaliacao="4,5" endereco="Grajau - são paulo" categorias="Celular" />
+                </div>
             </div>
-            <hr/>
+
             <Pagination />
             <Footer />
         </>
