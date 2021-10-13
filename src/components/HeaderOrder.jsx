06fb835708cd2aa4d4056562754a555fc07172ec
João Pedro Arnaut <joao.silva@bandtec.com.br>
2021-10-13 10:07:03 -0300

    Branch italo (#5)
    * Pagina de pedidos
    .
    ;
    
    * Alteracao navBar + page DetalhePedido
    
    Co-authored-by: italo-de-souza <italo.souza@bandtec.com.br>

---
+++ src/components/HeaderOrder.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+
+const HeaderOrder = ({ tituloPagina }) => {
+    return (
+        <>
+            <div className="pt-24 flex justify-around">
+                <div className="w-4/5">
+                    <div className="sm:w-full flex justify-between pb-3 mt-12 ">
+                        <button className="rounded-3xl px-5 bg-blue-dark w-40 hover:text-black hover:bg-blue duration-75 text-white text-2xl font-bold flex justify-around items-center">
+                            <FontAwesomeIcon icon={faArrowLeft} />Voltar</button>
+                        <p className="text-5xl font-bold">{tituloPagina}</p>
+                    </div>
+                    <hr />
+
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default HeaderOrder
