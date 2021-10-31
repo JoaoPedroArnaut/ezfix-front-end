657afc10ed85aa5c8875febbcbc10cf8ee303705
italo-de-souza <italo.souza@bandtec.com.br>
2021-10-31 13:31:13 -0300

    Botoes

--- src/components/Botao.jsx
+++ src/components/Botao.jsx
@@ -3,25 +3,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 
-const Botao = ({onClick, icone, text, estilo }) => {
-    switch (estilo) {
-        case 1:
-            return (
-                <button className="bg-blue w-full xl:w-60 xl:h-14 lg:w-56 lg:h-12 xl:text-lg sm:w-48 font-medium min-w-min p-2 sm:rounded-md hover:text-blue hover:bg-blue-light duration-75" onClick={onClick} >{text}</button>
-            )
+const Botao = ({ onClick, icone, text }) => {
 
-        case 2:
-            return (
-                <button className="rounded-3xl px-6 py-2  bg-blue w-52 text-white text-xl flex justify-around items-center hover:text-blue hover:bg-blue-light duration-75" onClick={onClick}>
-                    <FontAwesomeIcon icon={icone} />{text}</button>
-            )
-
-        default:
-            return (
-                <button className="bg-blue-dark w-24 min-w-min p-2 rounded-md text-white hover:bg-blue-light duration-75" onClick={onClick}>{text}</button>
-            )
-
-    }
+    return botoes = [
+        <button className="bg-blue w-full xl:w-60 xl:h-14 lg:w-56 lg:h-12 xl:text-lg sm:w-48 font-medium min-w-min p-2 sm:rounded-md hover:text-blue hover:bg-blue-light duration-75" onClick={onClick} >{text}</button>,
+        
+        <button className="rounded-3xl px-6 py-2  bg-blue w-52 text-white text-xl flex justify-around items-center hover:text-blue hover:bg-blue-light duration-75" onClick={onClick}>
+            <FontAwesomeIcon icon={icone} />{text}</button>,
+        
+        <button className="bg-blue-dark w-24 min-w-min p-2 rounded-md text-white hover:bg-blue-light duration-75" onClick={onClick}>{text}</button>
+    ]
 
 }
 
