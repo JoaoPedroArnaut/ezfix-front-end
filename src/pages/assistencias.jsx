89ace2b52c39c5d7fbcc12e39630bdbd30988808
JoaoPedroArnaut <joao.silva@bandtec.com.br>
2021-11-04 11:18:47 -0300

    deploy

--- src/pages/assistencias.jsx
+++ src/pages/assistencias.jsx
@@ -11,7 +11,7 @@ import Recomendadas from '../components/Recomendadas'
 const assistencias = () => {
     return (
         <>
-            <Navbar fixed={true} logado="true" usuario="Ítalo" imgPerfil="/PerfilUsuario.jpeg" page="" />
+            <Navbar login={["Login", "Criar conta"]} fixed={true} logado="true" usuario="Ítalo" imgPerfil="/PerfilUsuario.jpeg" page="" />
             <Recomendadas />
             <div className="flex flex-col items-center">
                 <div className="flex flex-col items-center sm:flex-row sm:justify-around w-full my-8">
