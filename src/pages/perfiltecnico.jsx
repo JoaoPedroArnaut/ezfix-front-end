88ffe4f077678cad9c2b66f460367f83dc5a2978
Ítalo de Souza <70718058+Italo-de-Souza@users.noreply.github.com>
2021-11-30 14:40:08 -0300

    Italo (#48)
    * Pagina de pedidos
    .
    ;
    
    * Alteracao navBar + page DetalhePedido
    
    * ajuste nos botões e na tela ConfirmacaoPedido
    
    * Pagina de detalhes
    
    * Merge remote-tracking branch 'origin/main' into branch_italo
    
    * Botoes
    
    * xablau
    
    * Xablau
    
    * Update Banner.jsx
    
    * tela pagamento
    
    * Componente card; Ajustes telas pedidos
    
    * ajuste navbar + header
    
    * modal
    
    * tela perfilUsuario
    
    * modal de filtragem mais ajustes
    
    * tela relatório pedido
    
    * modal novo endereço
    
    * tela PerfilAssistencia
    
    * sidebar tecnico
    
    * sidebar-tecnico
    
    * perfilAssistencia pronta
    
    * sidebar
    
    * ajuste sidebar
    
    * Tela de perfil do técnico
    
    * tela perfilTecnico pronta
    
    * Tela de login e cadastro do tecnico
    
    * mudanças card planos
    
    * teste
    
    * melhoria cardPlano
    
    * valor do plano alterado
    
    * Tela faq e tela 404
    
    * tela faq
    
    * tela mensagens ITALO
    
    * tela de mensagens
    
    * ajuste Index; tela mensagens do cliente
    
    * ajustes css
    
    * ajustes tela pedidos do tecnico
    
    * filtro de listagem pedidos
    
    * implementação tela perfilTecnico
    
    * ajuste TablePedido
    
    * TablePedido
    
    Co-authored-by: JoaoPedroArnaut <joao.silva@bandtec.com.br>
    Co-authored-by: Yuri-Cruz <yuri.cruz@bandtec.com.br>
    Co-authored-by: sergio.trindade <sergio.trindade@bandtec.com.br>

--- src/pages/perfiltecnico.jsx
+++ src/pages/perfiltecnico.jsx
@@ -1,16 +1,33 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/rules-of-hooks */
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import Input from '../components/Input';
+import { SessaoContext } from '../contexts/Sessao'
+import { api } from '../api/api'
+import { useRouter } from 'next/router'
 import SidebarTecnico from "../components/SidebarTecnico";
 import TecnicoAbaCertificado from '../components/TecnicoAbaCertificado';
 import TecnicoAbaContato from '../components/TecnicoAbaContato';
 import ModalNovoCert from '../components/ModalNovoCert'
 
 const perfiltecnico = () => {
-
+    const { user } = useContext(SessaoContext)
+    const router = useRouter();
     const [contCert, setContCert] = useState(true);
     const [modalNovoCert, setModalNovoCert] = useState(false);
+
+    function handlelUplod(e) {
+
+        var formdata = new FormData();
+        formdata.append("img", e[0], e[0].name);
+
+        api.post(`http://localhost:8080/assistencia/perfil/${user.id}`, formdata)
+            .then(res => {
+                router.reload();
+            }, err => {
+                console.log(err.response);
+            })
+    }
 
     return (
         <>
@@ -20,9 +37,13 @@ const perfiltecnico = () => {
                     <h1 className="text-blue-dark_light text-4xl font-bold">Perfil:</h1>
                     <div className="bg-blue-light mt-5 p-14 w-11/12 flex rounded-2xl filter shadow-inner drop-shadow-2xl  ">
                         <div className="flex flex-col items-center">
-                            <img src="./ThTecnologiaLogo.jpg" alt="Logo" width="200px" className="rounded-full" />
-                            <span className="text-3xl mt-2">Th Tecnologia</span>
-                            <Input label="Proprietário" value="Nome do mano" alternativoDois disabled={true} />
+                            <img src={`http://localhost:8080/assistencia/perfil/${user.id}`} alt="Logo" className="w-64 h-64 rounded-full" />
+                            <div className="text-base mt-5 mb-10">
+                                <label htmlFor="img" className="cursor-pointer bg-blue-dark hover:bg-blue-dark_light py-3 px-5 rounded-3xl text-white">Mudar Imagem</label>
+                                <input onChange={e => { handlelUplod(e.target.files) }} id="img" type="file" className="hidden" />
+                            </div>
+                            <span className="text-3xl font-semibold">{user.nomeFantasia}</span>
+                            <Input label="Proprietário" value="BiriJhonson" alternativoDois disabled={true} />
                             <Input label="CNPJ" value="CNPJ do mano" alternativoDois disabled={true} />
                         </div>
                         <div className="self-center border-r-2 h-96 mr-20 ml-20"></div>
@@ -37,10 +58,10 @@ const perfiltecnico = () => {
                                     : "bg-blue-dark text-white text-2xl font-medium py-2 px-6 rounded-full"}
                                     onClick={() => setContCert(false)}>Certificados</button>
                             </div>
-                            {contCert ? <TecnicoAbaContato /> : <TecnicoAbaCertificado setModalNovoCert={setModalNovoCert}/>}
+                            {contCert ? <TecnicoAbaContato /> : <TecnicoAbaCertificado setModalNovoCert={setModalNovoCert} />}
                         </div>
                     </div>
-                    {modalNovoCert && <ModalNovoCert setModalNovoCert={setModalNovoCert}/>}
+                    {modalNovoCert && <ModalNovoCert setModalNovoCert={setModalNovoCert} />}
                 </div>
 
             </div>
