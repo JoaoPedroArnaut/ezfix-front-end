efd0b81daadb8ef5786522f8415c31085dc86053
Ítalo de Souza <70718058+Italo-de-Souza@users.noreply.github.com>
2021-12-02 12:54:32 -0300

    Italo (#55)
    * Pagina de pedidos
    .
    ;
    
    * Alteracao navBar + page DetalhePedido
    
    * ajuste nos botões e na tela ConfirmacaoPedido
    
    * Pagina de detalhes
    
    * Merge remote-tracking branch 'origin/main' into branch_italo
    
    * Botoes
    
    * xablau
    
    * Xablau
    
    * Update Banner.jsx
    
    * tela pagamento
    
    * Componente card; Ajustes telas pedidos
    
    * ajuste navbar + header
    
    * modal
    
    * tela perfilUsuario
    
    * modal de filtragem mais ajustes
    
    * tela relatório pedido
    
    * modal novo endereço
    
    * tela PerfilAssistencia
    
    * sidebar tecnico
    
    * sidebar-tecnico
    
    * perfilAssistencia pronta
    
    * sidebar
    
    * ajuste sidebar
    
    * Tela de perfil do técnico
    
    * tela perfilTecnico pronta
    
    * Tela de login e cadastro do tecnico
    
    * mudanças card planos
    
    * teste
    
    * melhoria cardPlano
    
    * valor do plano alterado
    
    * Tela faq e tela 404
    
    * tela faq
    
    * tela mensagens ITALO
    
    * tela de mensagens
    
    * ajuste Index; tela mensagens do cliente
    
    * ajustes css
    
    * ajustes tela pedidos do tecnico
    
    * filtro de listagem pedidos
    
    * implementação tela perfilTecnico
    
    * ajuste TablePedido
    
    * TablePedido
    
    * Tela de acompanhamento Tecnico/Cliente; ModalEditaCertificado
    
    * Tela de acompanhamento Tecnico/Cliente; ModalEditaCertificado
    
    * Modal de avaliacao
    
    * Ajustes modalAvaliar
    
    Co-authored-by: JoaoPedroArnaut <joao.silva@bandtec.com.br>
    Co-authored-by: Yuri-Cruz <yuri.cruz@bandtec.com.br>
    Co-authored-by: sergio.trindade <sergio.trindade@bandtec.com.br>

--- src/components/tecnico/EtapaLinhaStatus.jsx
+++ src/components/tecnico/EtapaLinhaStatus.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const EtapaLinhaStatus = ({ checked }) => {
     if (checked) {
         return (
-            <div className="flex w-full justify-center items-center">
+            <div className="flex w-full justify-center mt-10 items-center">
                 <hr className="w-11/12 rounded-full text-blue-dark border-4" />
             </div>
         )
     } else {
         return (
-            <div className="flex w-full justify-center items-center">
+            <div className="flex w-full justify-center mt-10 items-center">
                 <hr className="w-11/12 rounded-full text-gray-blue border-4" />
             </div>
         )
